4a079925403eda5703bcbfa06e41552e56cdcaed
zjh <m18375326020_1@163.com>
2017-09-12 16:07:26 +0800

    init project

--- src/components/Main.js
+++ src/components/Main.js
@@ -20,17 +20,39 @@ imageDatas = (function genImageURL(imageDatasArr) {
   return imageDatasArr;
 })(imageDatas);
 
-
+var ImageFigure = React.createClass({
+  render: function() {
+    return(
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL}
+          alt={this.props.data.title}
+        />
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+});
 
 class AppComponent extends React.Component {
+
+  
   render() {
+    var controllerUnits = [],
+    imgFigures = [];
+
+    imageDatas.forEach(function(value){
+      imgFigures.push(<ImgFigures data={value}/>);
+    }); 
+
     return (
       <section className="stage">
         <section className="img-sec">
-
+          {imgFigures}
         </section>
         <nav className="controller-nav">
-          
+          {controllerUnits}
         </nav>
       </section>
     );
